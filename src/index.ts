7acf3f12f3ce2e2688b4fe0895bcf04c52cb258e
rootkc <info@kennethchristensen.dk>
2019-05-29 23:29:38 +0200

    add webpack with typescript config and prettier

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greeter(person: string) {
+  return 'Hello, ' + person;
+}
+
+let user = 'Jane User';
+console.log(user);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
